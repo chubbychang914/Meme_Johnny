9053e1d5f099cbc3c11aae73c14782aa6186676a
Johnny Chang <johnny22375@gmail.com>
2023-06-01 00:48:56 +0800

    fixed router btn component

--- src/components/navbar/RouterLinkBtn.vue
+++ src/components/navbar/RouterLinkBtn.vue
@@ -1,13 +1,10 @@
 <template>
   <div id="RouterLinkBtn" @click="handleClick">
-    <router-link class="router-link" :to="url">
-      <div class="urlName">{{ urlName }}</div>
-    </router-link>
+    <router-link class="router-link" :to="url">{{ urlName }}</router-link>
   </div>
 </template>
 
 <script setup>
-import { computed, defineProps } from 'vue';
 // props =======================================
 defineProps({
   url: {
@@ -16,7 +13,7 @@ defineProps({
   },
   urlName: {
     type: String,
-    default: "Home"
+    default: ""
   }
 })
 // methods =======================================
@@ -28,8 +25,6 @@ function handleClick() {
 </script>
 
 <style lang="scss" scoped>
-// 變數
-
 // 排版
 #RouterLinkBtn {
   display: flex;
@@ -37,15 +32,14 @@ function handleClick() {
 
 // 元件
 #RouterLinkBtn {
-  background-color: white;
-  border: 1px solid black;
-  padding: 10px 20px;
-
   .router-link {
+    display: flex;
+    background-color: white;
+    border: 1px solid black;
+    padding: 10px 20px;
     text-decoration: none;
     font-size: 30px;
     color: black;
   }
-
 }
 </style>
