d1d338f19faf59a380b4f0b6f036c7d116391481
Johnny <johnny22375@gmail.com>
2023-06-06 18:23:01 +0800

    added scrolltrigger panels for about page

--- src/views/web/AboutView.vue
+++ src/views/web/AboutView.vue
@@ -1,49 +1,17 @@
 <template>
   <div id="AboutView">
-    <NavBar />
-    <!-- <AboutSummary /> -->
-    <!-- <AboutExperience /> -->
-    <AboutSkills ref="aboutSkillsRef" />
+    <AboutPageCards />
   </div>
 </template>
 
 <script setup>
-// tools
-import { getCurrentInstance, onMounted, ref } from 'vue';
-import { useRouter, onBeforeRouteLeave } from 'vue-router';
-const { proxy: { $gsapPack } } = getCurrentInstance()
-const router = useRouter()
-// components
-import NavBar from '@/components/reusable/NavBar.vue'
-import AboutSummary from '@/components/about/AboutSummary.vue';
-import AboutExperience from '@/components/about/AboutExperience.vue';
-import AboutSkills from '@/components/about/AboutSkills.vue'
-
-
-const aboutSkillsRef = ref(null)
-// Animations ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
-const _animatePanelsEnter = () => {
-  const tl = $gsapPack.gsap.timeline({ paused: true })
-  tl.$gsapPack.scrollTrigger({
-    trigger: aboutSkillsRef.value,
-    markers: true,
-    start: "top 35%",
-    end: "top 1%",
-    scrub: true
-  })
-}
-
-onMounted(() => {
-  _animatePanelsEnter()
-})
+import AboutPageCards from "@/components/about/AboutPageCards.vue"
 </script>
 
 <style lang="scss" scoped>
 // 排版
 #AboutView {
-  display: grid;
-  grid-template-rows: 80px 1fr;
-  height: 100vh;
+  height: 140vh;
 }
 
 // 元件
