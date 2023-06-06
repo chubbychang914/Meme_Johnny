4f652b804f267ce62ad6d8170400eef5a260967c
Johnny <johnny22375@gmail.com>
2023-06-06 14:25:37 +0800

    removed unnecessary code

--- src/components/home/HomePageBg.vue
+++ src/components/home/HomePageBg.vue
@@ -1,26 +1,16 @@
 <template>
   <div id="HomePageBg">
-    <div class="bg-image">
-      <div class="space"></div>
-    </div>
     <!-- left side -->
     <div class="left-panel">
-      <!-- <div class="content-section">
-      </div> -->
-      <div class="triangle-section">
-        <img class="triangleSvg" src="src/assets/imgs/trapezoid.png" alt="triangle" />
-      </div>
+      <img class="trapezoidSvg" src="@/assets/svgs/trapezoid.svg" alt="triangle" />
     </div>
     <!-- middle -->
     <div class="middle-panel">
-      <img class="person-img" src="@/assets/svgs/personBg.png">
+      <img class="person-img" src="@/assets/imgs/personBg.png">
     </div>
     <!-- right side -->
     <div class="right-panel">
-      <div class="triangle-section">
-        <img class="triangleSvg" src="src/assets/imgs/trapezoid.png" alt="triangle" />
-      </div>
-      <!-- <div class="content-section"></div> -->
+      <img class="trapezoidSvg" src="@/assets/svgs/trapezoid.svg" alt="triangle" />
     </div>
   </div>
 </template>
@@ -32,15 +22,15 @@ const { proxy: { $gsapPack } } = getCurrentInstance() // 要引入這包才能�
 const EnterPageAnimationFlow = () => {
   const tl = $gsapPack.gsap.timeline()
   tl.add(_animatePanel().play())
-    .add(_animateBgLoop().play(), "<")
+    // .add(_animateBgLoop().play(), "<")
   return tl
 }
 // Animations ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
-const _animateBgLoop = () => {
-  const tl = $gsapPack.gsap.timeline({ paused: true, defaults: { repeat: -1 } })
-  tl.to('.space', { backgroundPosition: "-5000px 0px", duration: 25, ease: "linear" })
-  return tl
-}
+// const _animateBgLoop = () => {
+//   const tl = $gsapPack.gsap.timeline({ paused: true, defaults: { repeat: -1 } })
+//   tl.to('.space', { backgroundPosition: "-5000px 0px", duration: 25, ease: "linear" })
+//   return tl
+// }
 
 const _animatePanel = () => {
   const tl = $gsapPack.gsap.timeline({ paused: true, defaults: { duration: 1, ease: "power1.out" } })
@@ -57,9 +47,8 @@ onMounted(() => {
 
 <style lang="scss" scoped>
 // 變數
-$themeColor: #E4D00A;
-$contentWidthPercent: 50%;
-$triangleWidthPercent: 100% - $contentWidthPercent;
+// $contentWidthPercent: 50%;
+// $triangleWidthPercent: 100% - $contentWidthPercent;
 
 // 排版
 #HomePageBg {
@@ -72,50 +61,36 @@ $triangleWidthPercent: 100% - $contentWidthPercent;
 
 // 元件
 #HomePageBg {
-  .bg-image {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
+  // .bg-image {
+  //   position: absolute;
+  //   width: 100%;
+  //   height: 100%;
+  //   overflow: hidden;
 
-    .space {
-      image-rendering: pixelated;
-      // background: url("@/assets/imgs/spacebg.jpg") no-repeat 0 0 transparent;
-      background-repeat: repeat;
-      background-size: cover;
-      transform: rotate(-75deg);
-      width: 100%;
-      height: 100%;
-    }
-  }
-
+  //   .space {
+  //     image-rendering: pixelated;
+  //     background: url("@/assets/imgs/spacebg.jpg") no-repeat 0 0 transparent;
+  //     background-repeat: repeat;
+  //     background-size: cover;
+  //     transform: rotate(-75deg);
+  //     width: 100%;
+  //     height: 100%;
+  //   }
+  // }
+  // 左
   .left-panel {
-    display: flex;
-
-    .content-section {
-      width: $contentWidthPercent;
+    .trapezoidSvg {
+      position: absolute;
       height: 100vh;
-      background-color: $themeColor;
-    }
-
-    .triangle-section {
-      position: relative;
-      width: $triangleWidthPercent;
-      height: 100vh;
-
-      .triangleSvg {
-        position: absolute;
-        left: -1px;
-        height: 100vh;
-      }
     }
   }
 
+  // 人物
   .middle-panel {
     position: absolute;
     z-index: 2;
     left: 50%;
-    transform: translateX(-70%);
+    transform: translateX(-60%);
     bottom: 0;
     overflow: hidden;
 
@@ -124,28 +99,13 @@ $triangleWidthPercent: 100% - $contentWidthPercent;
     }
   }
 
+  // 右
   .right-panel {
-    display: flex;
-    justify-content: flex-end;
-
-    .content-section {
-      width: $contentWidthPercent;
+    .trapezoidSvg {
+      position: absolute;
+      right: 0;
       height: 100vh;
-      background-color: $themeColor;
-      display: flex;
-    }
-
-    .triangle-section {
-      position: relative;
-      width: $triangleWidthPercent;
-      height: 100vh;
-
-      .triangleSvg {
-        position: absolute;
-        right: -1px;
-        height: 100vh;
-        transform: rotate(180deg);
-      }
+      transform: rotate(180deg);
     }
   }
 }
---
+++ src/components/reusable/HomeNav.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="HomeNav">
+    
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+// 排版
+#HomeNav {}
+
+// 元件
+#HomeNav {}
+</style>
