ee49b4e560b4a4a41bec22dec8eaed7bd67588e3
Johnny <johnny22375@gmail.com>
2023-06-09 17:43:59 +0800

    before changing to flex

--- src/components/projects/ProjectsTemplate.vue
+++ src/components/projects/ProjectsTemplate.vue
@@ -1,16 +1,68 @@
 <template>
-  <div id="ProjectsTemplate">
+  <div id="ProjectsTemplate" :class="{ reverse: props.infoObj.reverseLayout }">
+    <div class="row">
+      <div class="projectPic">
+        <img :src=props.infoObj.imgUrl>
+      </div>
+    </div>
+    <div class="row">
+      <div class="projectDesc">
+        <div class="text"> {{ props.infoObj.description }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
+const props = defineProps({
+  infoObj: {
+    type: Object,
+    default: () => ({
+      title: "",
+      description: "Hello world",
+      imgUrl: "https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_1280.jpg",
+      reverseLayout: false
+    })
+  }
+})
 
 </script>
 
 <style lang="scss" scoped>
 // 排版
-#ProjectsTemplate {}
+#ProjectsTemplate {
+  width: 1000px;
+  height: 500px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+
+  .row {
+    background-color: blue;
+    outline: auto;
+    @extend .center;
+  }
+}
 
 // 元件
-#ProjectsTemplate {}
+#ProjectsTemplate {
+  .projectPic {
+    width: 100%;
+
+    img {
+      width: 100%;
+      height: auto;
+      object-fit: contain;
+    }
+  }
+
+  .projectDesc {}
+}
+
+.reverse {}
+
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
