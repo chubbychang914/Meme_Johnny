a9a3d011a3141e0f8a618ed40ac3f479df3adca5
Johnny <johnny22375@gmail.com>
2023-06-13 16:00:57 +0800

    added space ship svg

---
+++ src/components/home/SpaceBaseLayer.vue
@@ -0,0 +1,17 @@
+<template>
+#SpaceBaseLayer
+  p SpaceBaseLayer
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+// 排版
+#SpaceBaseLayer {
+}
+// 元件
+#SpaceBaseLayer {
+}
+</style>
