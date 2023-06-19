4c59f45ab146a687c129aa5117df2448a873e5e8
Johnny <johnny22375@gmail.com>
2023-06-19 20:02:54 +0800

    added moving bg

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="AboutView">
-    <h1>fist</h1>
+  <div id="AboutView" ref="aboutViewRef">
+    <!-- <h1>fist</h1>
     <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit, autem.</h1>
     <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit, autem.</h1>
     <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit, autem.</h1>
@@ -21,11 +21,42 @@
     <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit, autem.</h1>
     <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit, autem.</h1>
     <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit, autem.</h1>
-    <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit, autem.</h1>
+    <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit, autem.</h1> -->
   </div>
 </template>
 
 
 <script setup>
-import NavbarLayout from "@/components/layout/NavbarLayout.vue"
+import { onMounted, getCurrentInstance, ref } from 'vue';
+const { proxy: { $gsapPack } } = getCurrentInstance();
+
+const aboutViewRef = ref(null);
+
+let AnimateAboutViewBg = null;
+onMounted(() => {
+  const backgroundImageWidth = aboutViewRef.value.clientWidth;
+  AnimateAboutViewBg = $gsapPack.gsap.timeline({ paused: true, defaults: { repeat: -1 } })
+  AnimateAboutViewBg.to(aboutViewRef.value, {
+    duration: 60,
+    // backgroundPosition: `${backgroundImageWidth * 100}px 0px`,
+    backgroundPosition: "10000px 0px",
+    ease: "none"
+  })
+
+  AnimateAboutViewBg.play()
+})
+
+
 </script>
+
+<style lang="scss" scoped>
+#AboutView {
+  width: 100%;
+  min-height: 100vh;
+  background-color: yellow;
+  background-image: url("src/assets/imgs/about/aboutBg.jpeg");
+  background-position: center;
+  background-size: cover;
+  background-attachment: fixed;
+}
+</style>
