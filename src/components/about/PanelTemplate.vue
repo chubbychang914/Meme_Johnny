c60935f75be9bea3cc0622ec64a8b3635e357b6d
Johnny <johnny22375@gmail.com>
2023-06-19 14:32:57 +0800

    removed panel templates

--- src/components/about/PanelTemplate.vue
+++ src/components/about/PanelTemplate.vue
@@ -1,81 +1,25 @@
 <template>
-  <div id="AboutTemplate">
-    <div class="header">
-      <div class="icon">
-        <img :src="iconUrl">
-      </div>
-      <div class="title">
-        <div class="title-text">{{ props.title }}</div>
-      </div>
-    </div>
-    <div class="content">
-      <slot name="content"></slot>
-    </div>
+  <div id="PanelTemplate">
+    <slot></slot>
   </div>
 </template>
 
 <script setup>
-const props = defineProps({
-  title: {
-    type: String,
-    default: "title"
-  },
-  iconUrl: {
-    type: String,
-    default: ""
-  }
-})
 </script>
 
 <style lang="scss" scoped>
-$themeColor: #E4D00A;
-$iconDivSize: 100px;
 // 排版
 
-#AboutTemplate {
-  display: grid;
-  grid-template-columns: $iconDivSize 1fr;
-  height: 100%;
+#PanelTemplate {
+  width: 500px;
+  height: 500px;
+  background-color: red;
+  @include mobile-media{
+    width: 300px;
+    height: 300px;
+  }
 }
 
 // 元件
-#AboutTemplate {
-  .header {
-    background-color: $themeColor;
-    display: grid;
-    grid-template-rows: $iconDivSize 1fr;
-
-    // icon box
-    .icon {
-      // background-color: red;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-
-      img {
-        object-fit: contain;
-        width: calc($iconDivSize - 20px);
-      }
-    }
-
-    // title box
-    .title {
-      display: flex;
-      flex-direction: column;
-      justify-content: flex-end;
-      align-items: center;
-      padding-bottom: 20px;
-
-      .title-text {
-        // background-color: red;
-        font-size: 80px;
-        font-family: 'VT323', monospace;
-        transform: rotate(180deg);
-        writing-mode: vertical-lr;
-        text-orientation: mixed;
-
-      }
-    }
-  }
-}
+#PanelTemplate {}
 </style>
