4d9bc38b6ae03cb28a2abc57a7c7ae1b4757b415
Johnny <johnny22375@gmail.com>
2023-06-20 13:50:15 +0800

    now movable

--- src/components/about/ScrollComponent.vue
+++ src/components/about/ScrollComponent.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="ScrollComponent">
-    <div class="testContainer">
-      <div class="test" ref="testRef"></div>
-    </div>
+    <section class="section1">
+      <div class="container1">
+        <div class="box1 scrub" ref="box1Ref">1</div>
+      </div>
+    </section>
+    <section class="section2">
+    </section>d
   </div>
 </template>
 
@@ -10,18 +14,32 @@
 import { getCurrentInstance, onMounted, ref } from 'vue';
 const { proxy: { $gsapPack } } = getCurrentInstance()
 
-const testRef = ref(null);
-
-let AnimateTest = null;
+const box1Ref = ref(null);
 
 onMounted(() => {
-  AnimateTest = $gsapPack.gsap.to(testRef.value, {
-    paused: true,
-    duration: 1,
-    y: 500,
+  const tl = $gsapPack.gsap.timeline({
+    scrollTrigger: {
+      trigger: box1Ref.value,
+      markers: true,
+      start: 'top 35%',
+      end: 'top 1%',
+      scrub: true,
+    },
   })
 
-  AnimateTest.play()
+  tl.to(box1Ref.value, {
+    top: 0,
+    left: '50%',
+    xPercent: '-50',
+    // duration: 10,
+    position: 'absolute',
+    scale: 4
+  }).to('.box1', {
+    top: '100%',
+    yPercent: '-100',
+    // duration: 20,
+    position: 'absolute',
+  })
 })
 
 </script>
@@ -34,25 +52,35 @@ onMounted(() => {
   padding-top: 80px;
 
   // testing
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background-color: yellow;
-
-  .testContainer {
-    position: relative;
-    margin-bottom: 100px;
-    width: 50%;
-    height: 30%;
-    background-color: white;
-    border: 1px solid black;
+  .section1 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #fd89b9;
   }
 
-  .test {
+  .container1 {
+    position: relative;
+    margin-bottom: -100px;
+    width: 50%;
+    height: 30%;
+    background-color: #fff;
+    border: 1px solid #000;
+  }
+
+  .box1 {
     width: 100px;
     height: 100px;
-    background-color: blue;
+    background-color: #165e83;
+  }
+
+  .section2 {
+    display: flex;
+    justify-content: center;
+    padding: 200px 0;
+    background-color: #fd89b9;
+    // background-color: #1e2a60;
   }
 }
 </style>
