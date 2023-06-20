8e1cfe8e655aa66a6afd05c5d3e35adadb307926
Johnny <johnny22375@gmail.com>
2023-06-20 16:54:03 +0800

    put on hold

--- src/components/about/ScrollComponent.vue
+++ src/components/about/ScrollComponent.vue
@@ -1,9 +1,14 @@
 <template>
   <div id="ScrollComponent">
     <!-- intro -->
-    <section class="section section1">
-      <div class="container1">
+    <section class="section section1" ref="section1Ref">
+      <div class="container1" ref="container1Ref">
         <div class="box1 scrub" ref="box1Ref">1</div>
+      </div>
+    </section>
+    <section class="section section2" ref="section2Ref">
+      <div class="container1" ref="container1Ref">
+        <div class="box1">1</div>
       </div>
     </section>
     <!-- footer -->
@@ -19,6 +24,8 @@ import { getCurrentInstance, onMounted, ref } from 'vue';
 import FooterLayOut from '@/components/layout/FooterLayOut.vue';
 const { proxy: { $gsapPack } } = getCurrentInstance()
 
+const section1Ref = ref(null);
+const container1Ref = ref(null);
 const box1Ref = ref(null);
 
 onMounted(() => {
@@ -27,7 +34,7 @@ onMounted(() => {
       trigger: box1Ref.value,
       markers: true,
       start: '-80', // 從trigger的-80px開始執行
-      end: 'bottom',
+      // end: 'bottom top',
       scrub: true,
     },
   })
@@ -40,7 +47,7 @@ onMounted(() => {
   }).to(box1Ref.value, {
     position: 'absolute',
     top: '100%',
-    yPercent: '-100',
+    // yPercent: '-100',
   })
 })
 
@@ -82,7 +89,6 @@ onMounted(() => {
     justify-content: center;
     padding: 200px 0;
     background-color: #fd89b9;
-    // background-color: #1e2a60;
   }
 }
 </style>
