c8f6ec087c1cb7a33e3705abe79490582f053a25
Johnny Chang <johnny22375@gmail.com>
2023-06-21 15:42:43 +0800

    changed scroll component

--- src/components/about/ScrollComponent.vue
+++ src/components/about/ScrollComponent.vue
@@ -1,90 +1,16 @@
 <template>
   <div id="ScrollComponent">
-    <!-- intro -->
-    <section class="section section1" ref="section1Ref">
-      <div class="container1" ref="container1Ref">
-        <div class="box1 scrub" ref="box1Ref">AboutMe</div>
-      </div>
-    </section>
-    <!-- footer -->
-    <div class="footer">
-      <FooterLayOut />
-    </div>
+    <h1>Hello</h1>
   </div>
 </template>
 
-<script setup>
-import { getCurrentInstance, onMounted, ref } from 'vue';
+<script setup></script>
 
-import FooterLayOut from '@/components/layout/FooterLayOut.vue';
-const { proxy: { $gsapPack } } = getCurrentInstance()
-
-const section1Ref = ref(null);
-const container1Ref = ref(null);
-const box1Ref = ref(null);
-
-onMounted(() => {
-  const tl = $gsapPack.gsap.timeline({
-    scrollTrigger: {
-      trigger: box1Ref.value,
-      markers: true,
-      start: '-80', // 從trigger的-80px開始執行
-      // end: 'bottom top',
-      scrub: true,
-    },
-  })
-
-  tl.to(box1Ref.value, {
-    position: 'absolute',
-    top: 0,
-    left: '50%',
-    xPercent: '-50',
-  }).to(box1Ref.value, {
-    position: 'absolute',
-    top: '100%',
-    yPercent: '-100',
-  })
-})
-
-</script>
 
 <style lang="scss" scoped>
-#ScrollComponent {
+#ScrollComponent{
   width: 100%;
-  min-height: 200vh;
-  background-color: darkblue;
-  padding-top: 80px;
-
-  // testing
-  .section1 {
-    display: flex;
-    justify-content: center;
-    // align-items: center;
-    height: 100vh;
-    background-color: #fd89b9;
-    padding-top: 10vh;
-  }
-
-  .container1 {
-    position: relative;
-    margin-bottom: -100px;
-    width: 50%;
-    height: 70%;
-    background-color: #fff;
-    border: 1px solid #000;
-  }
-
-  .box1 {
-    width: 100px;
-    height: 100px;
-    background-color: #165e83;
-  }
-
-  .section2 {
-    display: flex;
-    justify-content: center;
-    padding: 200px 0;
-    background-color: #fd89b9;
-  }
+  min-height: 100vh;
+  background-color: yellow;
 }
 </style>
