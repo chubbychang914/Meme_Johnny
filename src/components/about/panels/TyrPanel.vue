80f36dc0d14febd339b6e488dd2f72c1437b82a4
Johnny Chang <johnny22375@gmail.com>
2023-06-26 23:58:52 +0800

    added style for tyr panel

--- src/components/about/panels/TyrPanel.vue
+++ src/components/about/panels/TyrPanel.vue
@@ -1,6 +1,23 @@
 <template>
   <div id="TyrPanel">
-    <h1>This is the tyr panel</h1>
+    <!-- 上半 -->
+    <div class="title">
+      <div class="companyIcon">
+        <img src="" alt="">
+      </div>
+      <div class="companyDesc">
+        <h1 class="companyName">Tyr
+          <span class="jobPeriod">2022.22</span>
+        </h1>
+        <h3 class="jobTitle">Frontend</h3>
+      </div>
+    </div>
+    <!-- 下半 -->
+    <div class="content">
+      <!-- <ul class="jobDesc">
+        <li v-for="(item, index) in props.jobInfoObj.companyDesc" :key="index">{{ item }}</li>
+      </ul> -->
+    </div>
   </div>
 </template>
 
@@ -14,5 +31,41 @@
   height: 100%;
   background-color: yellow;
   border-radius: 20px;
+
+  display: grid;
+  grid-template-rows: 1fr 3fr;
+
+  .title {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    background-color: red;
+
+    .companyIcon {}
+
+    .companyDesc {
+      display: grid;
+      grid-template-rows: 1fr 1fr;
+
+      .companyName {
+        display: flex;
+        align-items: center;
+
+        .jobPeriod {
+          font-size: 15px;
+        }
+      }
+
+      .jobTitle {
+        display: flex;
+        align-items: flex-start;
+      }
+    }
+  }
+
+  .content {
+    display: flex;
+    justify-content: center;
+    background-color: white;
+  }
 }
 </style>
