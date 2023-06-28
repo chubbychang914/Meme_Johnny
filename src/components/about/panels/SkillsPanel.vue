3184951f7927a7a8fecaa0e4cf692a22a1556654
Johnny Chang <johnny22375@gmail.com>
2023-06-28 16:09:25 +0800

    added tool boxes for each skill

--- src/components/about/panels/SkillsPanel.vue
+++ src/components/about/panels/SkillsPanel.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="SkillsPanel">
     <div class="title">Skills & Tools</div>
-    <div class="content"></div>
+    <div class="content">
+      <div class="tool-box" v-for="(item, index) in 9" :key="index"></div>
+    </div>
   </div>
 </template>
 
@@ -24,7 +26,7 @@
 
   .title {
     height: 20%;
-    background-color: red;
+    // background-color: red;
     font-size: 50px;
     @extend .center;
   }
@@ -32,7 +34,17 @@
   .content {
     height: 80%;
     background-color: blue;
-    @extend .center;
+    padding: 20px;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    flex-wrap: wrap;
+    gap: 20px;
+
+    .tool-box {
+      width: 100%;
+      height: 100%;
+      background-color: green;
+    }
   }
 }
 
