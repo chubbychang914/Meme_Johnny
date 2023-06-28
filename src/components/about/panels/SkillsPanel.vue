03d2c3aebc1d6cc2543c6bfa31cdafb3bd278fde
Johnny Chang <johnny22375@gmail.com>
2023-06-28 16:00:20 +0800

    layout of skills panel

--- src/components/about/panels/SkillsPanel.vue
+++ src/components/about/panels/SkillsPanel.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="SkillsPanel">
-    <h1>This is the skills panel</h1>
+    <div class="title">Skills & Tools</div>
+    <div class="content"></div>
   </div>
 </template>
 
@@ -10,9 +11,34 @@
 
 <style lang="scss" scoped>
 #SkillsPanel {
+  font-family: 'VT323', monospace;
   width: 100%;
   height: 100%;
-  background-color: brown;
+  background-color: khaki;
   border-radius: 20px;
+  border: 5px double black;
+  user-select: none;
+  // ================================
+  display: flex;
+  flex-direction: column;
+
+  .title {
+    height: 20%;
+    background-color: red;
+    font-size: 50px;
+    @extend .center;
+  }
+
+  .content {
+    height: 80%;
+    background-color: blue;
+    @extend .center;
+  }
+}
+
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
