a3fc25e3b6b23475b9299c7e8a2d2c77eb0b4a02
Johnny Chang <johnny22375@gmail.com>
2023-07-11 16:14:40 +0800

    added a loading page component

---
+++ src/components/layout/LoadingPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="LoadingPage">
+    <h1>This is the loading page before pics load</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+#LoadingPage {
+  background-color: blue;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
