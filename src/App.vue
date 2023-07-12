03c91c9bf8cc3de9fd7758b852e43ce78245785e
Johnny Chang <johnny22375@gmail.com>
2023-07-12 16:32:13 +0800

    adding mobile navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
-  <!-- <LoadingPage v-if="isLoading" /> -->
-  <!-- <div v-else> -->
-    <NavbarLayout ref="navbarLayoutRef" />
-    <RouterView />
-  <!-- </div> -->
+  <NavbarLayout ref="navbarLayoutRef" />
+  <RouterView />
 </template>
 
 <script setup>
@@ -11,42 +8,11 @@ import { onMounted, onUnmounted, ref, getCurrentInstance } from 'vue';
 import { RouterView } from 'vue-router'
 
 import NavbarLayout from './components/layout/NavbarLayout.vue';
-// import LoadingPage from './components/layout/LoadingPage.vue';
 import consoleLog from '@/tools/consoleLog.js'
 
 const { proxy: { $gsapPack } } = getCurrentInstance() // 要引入這包才能使用 gsap 的所有東西
 const navbarLayoutRef = ref(null)
 
-// state ==================================================
-// const imgList = ref([])
-// const isLoading = ref(true)
-// functions ==============================================
-// const loadImages = async () => {
-//   const images = [
-//     require('@/assets/img/image1.jpg').default,
-//     require('@/assets/img/image2.jpg').default,
-//     require('@/assets/img/image3.jpg').default,
-//   ];
-
-//   const imagePromises = images.map((image) => {
-//     return new Promise((resolve) => {
-//       const img = new Image();
-//       img.onload = resolve;
-//       img.src = image;
-//     });
-//   });
-
-//   await Promise.all(imagePromises);
-
-//   imgList.value = images.map((image) => {
-//     return {
-//       src: image,
-//       alt: 'Image description',
-//     };
-//   });
-
-//   isLoading.value = false;
-// };
 
 
 // gsap animations ========================================
@@ -65,7 +31,6 @@ onMounted(() => {
   AnimateNavbarEnter.play()
   // other functions
   consoleLog()
-  // loadImages()
 })
 
 onUnmounted(() => {
